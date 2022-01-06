b5b621d054646900c3ff8fd1ab61d223421791c5
Jay Patel <29896894+jayptl23@users.noreply.github.com>
2022-01-05 21:53:20 -0500

    hide api key, initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import {useEffect, useState} from 'react'
+
 const App = () => {
-	return <div className='App'>hello world</div>
+	useEffect(() => {
+		fetch(`https://api.nasa.gov/planetary/apod?count=3&api_key=${process.env.REACT_APP_API_KEY}`)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data)
+				setImages(data)
+			})
+	}, [])
+
+	const [images, setImages] = useState([])
+
+	return <div className='App'>{images && images.map(img => <p>{img.title}</p>)}</div>
 }
 
 export default App
