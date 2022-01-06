844cf38ec6a629b6bf837bc51245933e65174863
Jay Patel <29896894+jayptl23@users.noreply.github.com>
2022-01-06 13:15:46 -0500

    add like feature

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,27 @@
+import {useState} from 'react'
 import {HeartIcon} from '@heroicons/react/outline'
+import {HeartIcon as SolidHeartIcon} from '@heroicons/react/solid'
 
 const Card = ({image}) => {
+	const [isLiked, setIsLiked] = useState(false)
+
+	const handleLike = e => setIsLiked(prevState => !prevState)
+
 	const {title, url, hdurl, date, explanation} = image
 	return (
 		<article className='bg-gray-200 drop-shadow-md rounded-lg'>
-			<img className='mb-1 rounded-t-sm' src={url || hdurl} alt={title} />
+			<img className='mb-1 rounded-t-sm h-64 w-full object-cover' src={url || hdurl} alt={title} />
 			<div className='p-2'>
 				<div className='flex justify-between items-center px-2 mb-2'>
 					<div>
 						<p className='text-xs font-bold mb-1'>{title}</p>
 						<p className='text-xs font-semibold text-gray-800'>{date}</p>
 					</div>
-					<HeartIcon className='h-7 cursor-pointer text-gray-400 hover:scale-110 hover:text-red-500' />
+					{isLiked ? (
+						<SolidHeartIcon className='h-7 w-7 cursor-pointer text-red-500' onClick={handleLike} />
+					) : (
+						<HeartIcon className='h-7 w-7 cursor-pointer text-gray-400 hover:scale-110 hover:text-red-500' onClick={handleLike} />
+					)}
 				</div>
 				<p className='text-xs px-2'>{explanation}</p>
 			</div>
