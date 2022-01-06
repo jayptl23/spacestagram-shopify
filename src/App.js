e0d01abfada74a299445a34a4bc4501b10c74781
Jay Patel <29896894+jayptl23@users.noreply.github.com>
2022-01-05 21:59:13 -0500

    create func to fetch images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import {useEffect, useState} from 'react'
+import Card from './components/Card'
+
+const url = `https://api.nasa.gov/planetary/apod?count=3&api_key=${process.env.REACT_APP_API_KEY}`
+
+async function getImages() {
+	try {
+		const response = await fetch(url)
+		const data = await response.json()
+		console.log(data)
+		return data
+	} catch (error) {
+		console.log(error)
+	}
+}
 
 const App = () => {
 	useEffect(() => {
-		fetch(`https://api.nasa.gov/planetary/apod?count=3&api_key=${process.env.REACT_APP_API_KEY}`)
-			.then(response => response.json())
-			.then(data => {
-				console.log(data)
-				setImages(data)
-			})
+		getImages().then(images => setImages(images))
 	}, [])
 
 	const [images, setImages] = useState([])
 
-	return <div className='App'>{images && images.map(img => <p>{img.title}</p>)}</div>
+	return <div className='App'>{images && images.map(img => <Card />)}</div>
 }
 
 export default App
